32d3f76b11f59d47574b4af6970f9f481621a5de
renekrewinkel <rene@studiokrijst.com>
2023-02-09 09:53:54 +0100

    clean

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-import { useState, useEffect } from 'react'
-
-import { fetchData } from './lib'
-import { DataContext } from './context'
-
-import Overview from './components/pages/Overview'
-
 const App = () => {
-  const [data, setData] = useState([])
-  const [full, setFull] = useState([])
-  const [isLoaded, setLoaded] = useState(false)
 
-  const resetFilter = () => {
-    setData(full)
-  }
-  const filterPerson = (search) => {
-    const result = data.filter(
-      (item) =>
-        item.firstname.toUpperCase().includes(search.toUpperCase()) ||
-        item.lastname.toUpperCase().includes(search.toUpperCase())
-    )
-    setData(result)
-  }
+  return (<div></div>)
 
-  useEffect(() => {
-    fetchData().then((result) => {
-      setFull(result.data)
-      setData(result.data)
-      setLoaded(true)
-    })
-  }, [])
-
-  return (
-    <DataContext.Provider value={{ data, filterPerson, resetFilter }}>
-      {!isLoaded ? <h1>Spinner</h1> : <Overview />}
-    </DataContext.Provider>
-  )
 }
 
 export default App
